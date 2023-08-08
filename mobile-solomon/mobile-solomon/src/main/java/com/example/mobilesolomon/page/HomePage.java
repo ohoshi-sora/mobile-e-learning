99f4b5602494b9ccd4186da97e6b5f3358c928f8
showayamada <yamasho082433@gmail.com>
2023-08-08 13:37:37 +0900

    pomファイルを編集

---
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/page/HomePage.java
@@ -0,0 +1,12 @@
+package com.example.mobilesolomon.page;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@WicketHomePage
+@MountPath("Home")
+public class HomePage {
+    public HomePage(){
+    }
+}
