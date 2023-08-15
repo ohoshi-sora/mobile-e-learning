912d8b12ffe2acd8c598290d72eaf513df3532ab
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2023-08-15 21:13:11 +0900

    WebPage間を移動する 01 WebPageクラスのサブクラスを作成までやった

---
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/page/HintMakerPage.java
@@ -0,0 +1,17 @@
+package com.example.mobilesolomon.page;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+
+// ヒントを作るページ
+@MountPath("HintMaker")
+public class HintMakerPage extends WebPage {
+
+    public HintMakerPage() {
+
+    }
+
+}
+
+//WebPage間を移動する 01 WebPageクラスのサブクラスを作成までやった
