17ad1b9c5d99122c2faf568381c7a262509581a2
showayamada <yamasho082433@gmail.com>
2023-08-30 15:54:46 +0900

    api叩いてみたけどエラー

--- mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/HintService.java
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/HintService.java
@@ -1,6 +1,14 @@
 package com.example.mobilesolomon.service;
 
 import com.example.mobilesolomon.data.HintApiReader;
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import javax.print.attribute.standard.Media;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HintService {
     private String API_KEY;
@@ -13,8 +21,11 @@ public class HintService {
 　　レスポンスをうけとる
  　　*/
     public String HintService() {
+
         HintApiReader apiReader = new HintApiReader();
         this.API_KEY = apiReader.getAPI_KEY();
+        //System.out.println(API_KEY); debug
+
 
         /*
         HintPromptMaker promptMaker = new HintPromptMaker();
@@ -23,6 +34,47 @@ public class HintService {
          */
 
         //ここをかく　プロンプトは違うクラスで加工する？
+        this.prompt = "こんにちは、chatGPT。これはテストだよ。";
+
+        // HTTPクライアントのやつ
+        OkHttpClient client = new OkHttpClient();
+        // json扱うためのインスタンス
+        Gson gson = new Gson();
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("prompt", this.prompt);
+        requestBody.put("max_tokens", 100); //これはどうしましょう
+        requestBody.put("n", 1); // これはなにかもわかんない
+        requestBody.put("stop",null); // これもなんだろ
+        requestBody.put("api_key", API_KEY);
+
+        String jsonRequestBody = gson.toJson(requestBody);
+        RequestBody body = RequestBody.create(jsonRequestBody, MediaType.parse("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/engines/davinci-codex/completions")
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if(!response.isSuccessful()) {
+                throw new IOException("Unexpected code" + response);
+            }
+
+            String jsonResponse = response.body().string();
+            Map<String, Object> apiResponse = gson.fromJson(jsonResponse, Map.class);
+            String generatedText = ((List<Map<String, Object>>) apiResponse.get("choices"))
+                    .get(0)
+                    .get("text")
+                    .toString();
+            System.out.println("Generated Text : " + generatedText);
+
+            this.hint_madeByGPT = generatedText;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         return hint_madeByGPT;
         // 作られたヒントをreturnすることで、hintLogクラスでこのクラスのインスタンスを使うことでデータをlogにいれて
@@ -38,5 +90,11 @@ public class HintService {
 //        System.out.println(apiReader.getAPI_KEY());
 //    }
 
+    // debug用　コンソールにgptのレスポンスが表示されるはず
+    // できないいいいいいいいいいいいいい
+    public static void main(String[] args) {
+        HintService h = new HintService();
+        System.out.println(h.HintService());
+    }
 
 }
