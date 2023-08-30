df87ab944c17101ce4d0d6f1fadc6f7cc05b70d8
showayamada <yamasho082433@gmail.com>
2023-08-30 14:50:04 +0900

    envファイルからkeyの呼び出しするApiReaderクラスを作成

--- mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/HintService.java
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/HintService.java
@@ -1,5 +1,42 @@
 package com.example.mobilesolomon.service;
 
+import com.example.mobilesolomon.data.HintApiReader;
+
 public class HintService {
+    private String API_KEY;
+    private String prompt; // ChatGPTに送るプロンプト
+    private String hint_madeByGPT; // ChatGPTから返ってきたヒント
+
+
+    /*
+　　ここでapiをたたく、
+　　レスポンスをうけとる
+ 　　*/
+    public String HintService() {
+        HintApiReader apiReader = new HintApiReader();
+        this.API_KEY = apiReader.getAPI_KEY();
+
+        /*
+        HintPromptMaker promptMaker = new HintPromptMaker();
+        this.prompt = promptMaker.getPrompt();
+        こんな感じで
+         */
+
+        //ここをかく　プロンプトは違うクラスで加工する？
+
+        return hint_madeByGPT;
+        // 作られたヒントをreturnすることで、hintLogクラスでこのクラスのインスタンスを使うことでデータをlogにいれて
+        //そこからいろいろつかうようにする
+    }
+
+
+
+
+// debug用　APIキーがゲッターで呼び出せることを確認できた
+//    public static void main(String[] args) {
+//        HintAPIReader apiReader = new HintAPIReader();
+//        System.out.println(apiReader.getAPI_KEY());
+//    }
+
 
 }
