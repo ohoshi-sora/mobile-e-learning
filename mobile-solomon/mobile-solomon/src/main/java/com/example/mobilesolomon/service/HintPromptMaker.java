74345f18e7d80b2ad71b2a5cfaa896f41d121901
showayamada <yamasho082433@gmail.com>
2023-08-31 22:15:19 +0900

    promptMakerの試作

--- mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/HintPromptMaker.java
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/HintPromptMaker.java
@@ -1,5 +1,21 @@
 package com.example.mobilesolomon.service;
 
+import com.example.mobilesolomon.presentation.HintMakerPage;
+
 public class HintPromptMaker {
 
+    private String prompt;
+    private String userInput;
+
+    public HintPromptMaker() {
+        HintMakerPage hintMaker = new HintMakerPage();
+        this.userInput = hintMaker.getQuestion();
+
+        this.prompt = "以下の問題のヒントを作成して\n" + userInput;
+
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
 }
