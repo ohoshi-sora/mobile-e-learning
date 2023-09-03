ae85439de509973c2b1721ee077d8fcf94ad386e
showayamada <yamasho082433@gmail.com>
2023-09-04 00:04:00 +0900

    アイウエの答えをjavaで受けとる処理を追加したかった

--- mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/presentation/HintMakerPage.java
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/presentation/HintMakerPage.java
@@ -1,13 +1,19 @@
 package com.example.mobilesolomon.presentation;
 
 import com.example.mobilesolomon.service.HintService;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.PropertyModel;
 import org.wicketstuff.annotation.mount.MountPath;
+
+import java.util.Arrays;
 
 // 問題や解答を入力して、ボタンを押すとヒントを製作できる
 
@@ -16,6 +22,8 @@ public class HintMakerPage extends WebPage {
 
     /**入力された問題文**/
     private String question;
+    /**選択された答えアイウエ**/
+    private String answer;
 
     public HintMakerPage() {
         var toHintPreview = new BookmarkablePageLink<>("toHintPreview", HintPreviewPage.class);
@@ -26,6 +34,20 @@ public class HintMakerPage extends WebPage {
         Form<HintMakerPage> form = new Form<>("form", new CompoundPropertyModel<>(this));
         TextArea<String> textField = new TextArea<>("question");
         form.add(textField);
+
+//        DropDownChoice<String> dropdown = new DropDownChoice<>("select", new PropertyModel<>(this, "select"), Arrays.asList("a", "i", "u", "e"));
+//        add(dropdown);
+//        dropdown.add(new AjaxFormComponentUpdatingBehavior("change") {
+//            @Override
+//            protected void onUpdate(AjaxRequestTarget target) {
+//                // 選択された値をここで処理
+//                String selectedValue = ;
+//                // 選択された値を使用して必要な処理を実行
+//                System.out.println(selectedValue);
+//            }
+//        });
+
+
 
         form.add(new SubmitButton("submit"));
         add(form);
