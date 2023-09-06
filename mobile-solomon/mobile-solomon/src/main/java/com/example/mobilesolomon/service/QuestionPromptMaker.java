577a9e5d2f65f9de54afa1339bead692b5c9192a
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2023-09-07 01:34:03 +0900

    questionpromptmakerを作っている途中

---
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/QuestionPromptMaker.java
@@ -0,0 +1,12 @@
+package com.example.mobilesolomon.service;
+
+public class QuestionPromptMaker {
+
+    private String prompt;
+    private String userInput;
+
+    public QuestionPromptMaker() {
+
+
+    }
+}
