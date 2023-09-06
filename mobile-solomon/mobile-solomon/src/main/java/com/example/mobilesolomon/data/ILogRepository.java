f89abccb52df55693e7144ffd76564cc666ae91f
showayamada <yamasho082433@gmail.com>
2023-09-06 22:24:08 +0900

    sql追加（データ層の編集）

--- mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/data/ILogRepository.java
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/data/ILogRepository.java
@@ -1,6 +1,6 @@
 package com.example.mobilesolomon.data;
 
-public interface IHintLogRepository {
+public interface ILogRepository {
     /**
      * ユーザー名とパスワードをquestionテーブルに記録する
      *
@@ -11,6 +11,21 @@ public interface IHintLogRepository {
      * @return データベースの更新行数
      */
     public int insert(int num, String question, String answer, String hint);
+
+    /**
+     * 次に登録する通し番号を読み込む
+     *
+     * @return 通し番号の最後 + 1　の数字
+     */
+    public int selectMaxNum();
+
+    /**
+     *　削除する
+     * @pram num 通し番号
+     * @return データベースの削除行数
+     */
+    public int delete(int num);
+
 
 }
 
--- mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/data/LogRepository.java
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/data/LogRepository.java
@@ -5,22 +5,34 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class HintLogRepository implements IHintLogRepository{
+public class LogRepository implements ILogRepository {
 
     // spring Frameworkのデータベース接続ライブラリを利用する
     private final JdbcTemplate jdbcTemplate;
 
     // Spring Frameworkのデータベース接続ライブラリを初期化する
     @Autowired
-    public HintLogRepository(JdbcTemplate jdbcTemplate) {
+    public LogRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public int insert(int num, String question, String answer, String hint) {
         var sql = "insert into question values (?, ?, ?, ?)";
-        var n = jdbcTemplate.update(sql, num, question,answer, hint);
+        var n = jdbcTemplate.update(sql, num, question, answer, hint);
         return n;
+    }
+    @Override
+    public int selectMaxNum(){
+        var sql = "select max(NUM) from question";
+        return jdbcTemplate.queryForObject(sql,Integer.class);
+    }
+
+    @Override
+    public int delete(int num) {
+        String sql = "DELETE FROM question WHERE NUM = ?";
+        int deletedRows = jdbcTemplate.update(sql, num);
+        return deletedRows;
     }
 
 }
