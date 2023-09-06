1c2822b946b36b8351355226fb3e941884e5acbc
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2023-09-06 22:09:57 +0900

    questionserviceを作った

---
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/service/QuestionService.java
@@ -0,0 +1,17 @@
+package com.example.mobilesolomon.service;
+
+import org.springframework.stereotype.Service;
+
+// 問題と解答、ヒントを入力したら似たような問題を自動生成してくれるシステム
+
+@Service
+public class QuestionService {
+
+
+
+    // 問題文と解答、ヒントをデータベースから引っ張り出す
+
+
+
+
+}
