d7778f126b9b47fe738c749d6fb7f290382d3925
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2023-09-07 02:13:30 +0900

    QuestionRegistrationPage.htmlの作成

---
+++ mobile-solomon/mobile-solomon/src/main/java/com/example/mobilesolomon/presentation/question/QuestionRegistrationPage.java
@@ -0,0 +1,9 @@
+package com.example.mobilesolomon.presentation.question;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@MountPath("QuestionRegistration")
+public class QuestionRegistrationPage extends WebPage {
+
+}
